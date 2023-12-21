41111fb87f26b409e6a3e138ec50d29b835b96e6
MoonHo <75512752+monho@users.noreply.github.com>
2023-12-21 17:33:00 +0900

    [ADD] 컴포넌트 구조 생성
    [ADD] 컴포넌트 구조 생성

---
+++ client/src/components/header/SearchForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function SearchForm() {
+    return (
+        <Main_Warp>
+            <MaintenaceManageHeader />
+            <MaintenanceManageBody />
+        </Main_Warp>
+
+    );
+}
+export default SearchForm;
