3278e6c1b0bf2bd04759a7dc38a5f0b3f399b14f
MoonHo <75512752+monho@users.noreply.github.com>
2023-12-22 11:28:03 +0900

    [ADD]Contnet 컴포넌트 결합
    [ADD]Contnet 컴포넌트 결합

--- client/src/components/content/RankChart.js
+++ client/src/components/content/RankChart.js
@@ -1,39 +1,26 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
 
-const FormContainer = styled.form`
-    display: flex;
-    width: 500px;
-    height: 44px;
-    justify-content: flex-end;
-    align-items: center;
-    border-radius: 20px;
-    border: 1px solid #8B8B8B;
-    background: #FFF;
-    margin-top:15px;
+const ChartTable = styled.form`
+    width: 530px;
+    border-collapse: collapse;
 `;
 
+const TableHeade = styled.thead`
+    background: rgb(45, 47, 55);
+`;
+const HeaderItem = styled.tr`
+    display: flex;
+`;
 
 function RankChart() {
-    const [inputValue, setInputValue] = useState('');
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // 여기에서 입력값을 사용하거나 필요한 작업을 수행합니다.
-      console.log('Submitted:', inputValue);
-    };
     return (
-        <FormContainer onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          placeholder="구단주명 입력"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <Button type="submit">
-         <i class="fas fa-search"></i>
-        </Button>
-      </FormContainer>
+        <ChartTable>
+            <TableHeade>
+                <HeaderItem>순위</HeaderItem>
+                <HeaderItem>구단주명</HeaderItem>
+            </TableHeade>
+        </ChartTable>
     );
 }
-export default SearchForm;
+export default RankChart;
