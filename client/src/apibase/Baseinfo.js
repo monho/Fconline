3cc7d0ffddc1b6de8b9af48c80f6bf49ee80b3fe
monho <75512752+monho@users.noreply.github.com>
2024-01-15 20:53:50 +0900

    Update Baseinfo.js

--- client/src/apibase/Baseinfo.js
+++ client/src/apibase/Baseinfo.js
@@ -1,5 +1,4 @@
-const API_KEY =
-  "test_e9335a6787866a3cc9067b9336d410d9deda234d1ecd92a9b62e5b50be43978e09a938e49b96a3c2f6bbfda5e4ea66eb";
+const API_KEY = "";
 
 export const BaseApiUrl = {
   baseURL: "https://open.api.nexon.com",
